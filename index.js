e14a330c1582f44ab3a884f286e0c7e0d312b600
Jennifer Dobak <jdobak1@gmail.com>
2018-05-07 14:39:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-
-  this.doCharity = function(){
-    return "I like to help people."
-  }
-
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
-
-  this.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
 }
 
 BoardMember.prototype.veto = function(){
@@ -24,9 +12,12 @@ BoardMember.prototype.approve = function(){
   return 'You can do that!'
 }
 
-BoardMember.prototype.approve = function() {
+BoardMember.prototype.doCharity = function() {
+  return "I like to help people."
 }
-BoardMember.prototype.approve = function() {
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
-BoardMember.prototype.approve = function() {
+BoardMember.prototype.releasePressStatement = function() {
+  return "You will see great things from Scuber."
 }
